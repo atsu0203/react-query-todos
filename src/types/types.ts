fbd7b41039611fc8dc9c5ab06561f293b52394f0
atsu <atsu0203@gmail.com>
2024-10-02 23:08:40 +0900

    add: todoSlice

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,20 @@ export interface Task {
     created_at: string;
     updated_at: string;
 }
+
+export interface Tag {
+    id: number;
+    name: string;
+}
+
+export interface EditTask {
+    id: number;
+    title: string;
+    tag: number;
+}
+
+export interface EditTag {
+    id: number;
+    name: string;
+}
+
